ffcde01a1528c8be00d41cd0625f17172570d1d7
Quyse Lert <quyse0@gmail.com>
2018-08-01 17:09:21 +0300

    support --continue for elastic output

--- coinmetrics-export/CoinMetrics/Export/Storage/Elastic.hs
+++ coinmetrics-export/CoinMetrics/Export/Storage/Elastic.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedLists, OverloadedStrings #-}
 
 module CoinMetrics.Export.Storage.Elastic
 	( ElasticExportStorage()
@@ -9,15 +9,42 @@ import Control.Monad
 import qualified Data.Aeson as J
 import qualified Data.Aeson.Types as J
 import qualified Data.ByteString.Lazy as BL
+import qualified Data.Text.Encoding as T
 import qualified Network.HTTP.Client as H
 
 import CoinMetrics.Export.Storage
 import CoinMetrics.Unified
 
+-- Elastic storage supports only unified export for now.
+
 newtype ElasticExportStorage = ElasticExportStorage ExportStorageOptions
 
 instance ExportStorage ElasticExportStorage where
 	initExportStorage = return . ElasticExportStorage
+
+	getExportStorageMaxBlock (ElasticExportStorage ExportStorageOptions
+		{ eso_httpManager = httpManager
+		, eso_destination = destination
+		, eso_table = table
+		}) = Just $ do
+		httpRequest <- H.parseRequest destination
+		response <- H.responseBody <$> H.httpLbs httpRequest
+			{ H.method = "GET"
+			, H.requestHeaders = [("Content-Type", "application/json")]
+			, H.path = "/" <> T.encodeUtf8 table <> "/_search"
+			, H.requestBody = H.RequestBodyLBS $ J.encode $ J.Object
+				[ ("size", J.Number 0)
+				, ("track_total_hits", J.Bool False)
+				, ("aggs", J.Object
+					[ ("max_height", J.Object
+						[ ("max", J.Object
+							[ ("field", J.String "height")
+							])
+						])
+					])
+				]
+			} httpManager
+		either fail return . J.parseEither ((J..:? "value") <=< (J..: "max_height") <=< (J..: "aggregations")) =<< either fail return (J.eitherDecode response)
 
 	writeExportStorage (ElasticExportStorage options@ExportStorageOptions
 		{ eso_httpManager = httpManager
