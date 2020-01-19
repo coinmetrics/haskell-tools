c1673dc8cc6f6665916a392683ec0b3c67f7efb5
Quyse Lert <quyse0@gmail.com>
2020-01-19 15:51:20 +0300

    dockerless push into registry

--- default.nix
+++ default.nix
@@ -1,58 +1,60 @@
 { pkgs }:
 rec {
-	hanalytics = pkgs.fetchFromGitHub {
-		owner = "quyse";
-		repo = "hanalytics";
-		rev = "dbe294c4c1524268683764c3cd3ec947105359c9";
-		sha256 = "0wpib53843xggnd70vxxivmv9cd0gqgfpia3v3f81q0vdf8mz61k";
-	};
+  hanalytics = pkgs.fetchFromGitHub {
+    owner = "quyse";
+    repo = "hanalytics";
+    rev = "dbe294c4c1524268683764c3cd3ec947105359c9";
+    sha256 = "0wpib53843xggnd70vxxivmv9cd0gqgfpia3v3f81q0vdf8mz61k";
+  };
 
-	packages = pkgs.haskellPackages.override {
-		overrides = pkgs.lib.composeExtensions (pkgs.haskell.lib.packageSourceOverrides {
-			coinmetrics = ./coinmetrics;
-			coinmetrics-all-blockchains = ./coinmetrics-all-blockchains;
-			coinmetrics-binance = ./coinmetrics-binance;
-			coinmetrics-bitcoin = ./coinmetrics-bitcoin;
-			coinmetrics-cardano = ./coinmetrics-cardano;
-			coinmetrics-cosmos = ./coinmetrics-cosmos;
-			coinmetrics-eos = ./coinmetrics-eos;
-			coinmetrics-ethereum = ./coinmetrics-ethereum;
-			coinmetrics-export = ./coinmetrics-export;
-			coinmetrics-grin = ./coinmetrics-grin;
-			coinmetrics-iota = ./coinmetrics-iota;
-			coinmetrics-monero = ./coinmetrics-monero;
-			coinmetrics-monitor = ./coinmetrics-monitor;
-			coinmetrics-nem = ./coinmetrics-nem;
-			coinmetrics-neo = ./coinmetrics-neo;
-			coinmetrics-ripple = ./coinmetrics-ripple;
-			coinmetrics-stellar = ./coinmetrics-stellar;
-			coinmetrics-storage = ./coinmetrics-storage;
-			coinmetrics-tendermint = ./coinmetrics-tendermint;
-			coinmetrics-tezos = ./coinmetrics-tezos;
-			coinmetrics-tron = ./coinmetrics-tron;
-			coinmetrics-waves = ./coinmetrics-waves;
+  packages = pkgs.haskellPackages.override {
+    overrides = pkgs.lib.composeExtensions (pkgs.haskell.lib.packageSourceOverrides {
+      coinmetrics = ./coinmetrics;
+      coinmetrics-all-blockchains = ./coinmetrics-all-blockchains;
+      coinmetrics-binance = ./coinmetrics-binance;
+      coinmetrics-bitcoin = ./coinmetrics-bitcoin;
+      coinmetrics-cardano = ./coinmetrics-cardano;
+      coinmetrics-cosmos = ./coinmetrics-cosmos;
+      coinmetrics-eos = ./coinmetrics-eos;
+      coinmetrics-ethereum = ./coinmetrics-ethereum;
+      coinmetrics-export = ./coinmetrics-export;
+      coinmetrics-grin = ./coinmetrics-grin;
+      coinmetrics-iota = ./coinmetrics-iota;
+      coinmetrics-monero = ./coinmetrics-monero;
+      coinmetrics-monitor = ./coinmetrics-monitor;
+      coinmetrics-nem = ./coinmetrics-nem;
+      coinmetrics-neo = ./coinmetrics-neo;
+      coinmetrics-ripple = ./coinmetrics-ripple;
+      coinmetrics-stellar = ./coinmetrics-stellar;
+      coinmetrics-storage = ./coinmetrics-storage;
+      coinmetrics-tendermint = ./coinmetrics-tendermint;
+      coinmetrics-tezos = ./coinmetrics-tezos;
+      coinmetrics-tron = ./coinmetrics-tron;
+      coinmetrics-waves = ./coinmetrics-waves;
 
-			hanalytics-avro = hanalytics + "/hanalytics-avro";
-			hanalytics-base = hanalytics + "/hanalytics-base";
-			hanalytics-bigquery = hanalytics + "/hanalytics-bigquery";
-			hanalytics-postgres = hanalytics + "/hanalytics-postgres";
+      hanalytics-avro = "${hanalytics}/hanalytics-avro";
+      hanalytics-base = "${hanalytics}/hanalytics-base";
+      hanalytics-bigquery = "${hanalytics}/hanalytics-bigquery";
+      hanalytics-postgres = "${hanalytics}/hanalytics-postgres";
 
-			diskhash = "0.0.4.0";
-		}) (self: super: with pkgs.haskell.lib; {
-			diskhash = dontCheck super.diskhash;
-		});
-	};
+      diskhash = "0.0.4.0";
+    }) (self: super: with pkgs.haskell.lib; {
+      diskhash = dontCheck super.diskhash;
+    });
+  };
 
-	bins = with packages; [ coinmetrics-export coinmetrics-monitor ];
+  bins = with packages; [ coinmetrics-export coinmetrics-monitor ];
 
-	env = pkgs.buildEnv {
-		name = "haskell-tools";
-		paths = bins;
-	};
+  env = pkgs.buildEnv {
+    name = "haskell-tools";
+    paths = bins;
+  };
 
-	image = pkgs.dockerTools.buildImage {
-		name = "coinmetrics/haskell-tools";
-		tag = "latest";
-		contents = env;
-	};
+  image = { name, tag }: pkgs.dockerTools.buildImage {
+    inherit name tag;
+    config = {
+      Env = [ "PATH=${env}/bin" ];
+      User = "1000:1000";
+    };
+  };
 }
