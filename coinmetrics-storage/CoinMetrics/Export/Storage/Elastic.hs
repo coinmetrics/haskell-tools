7cc7eb808977ae37e02397231608b16115df2d71
Quyse Lert <quyse0@gmail.com>
2020-03-30 22:33:58 +0300

    retry failed blocks on insertion to elasticsearch

--- coinmetrics-storage/CoinMetrics/Export/Storage/Elastic.hs
+++ coinmetrics-storage/CoinMetrics/Export/Storage/Elastic.hs
@@ -9,6 +9,7 @@ import Control.Monad
 import qualified Data.Aeson as J
 import qualified Data.Aeson.Types as J
 import qualified Data.ByteString.Lazy as BL
+import qualified Data.Text as T
 import qualified Data.Text.Encoding as T
 import qualified Network.HTTP.Client as H
 
@@ -47,19 +48,30 @@ instance ExportStorage ElasticExportStorage where
 
   writeExportStorage (ElasticExportStorage options@ExportStorageOptions
     { eso_httpManager = httpManager
+    , eso_upsert = upsert
     }) ExportStorageParams
     { esp_destination = destination
     } packs = do
     httpRequest <- H.parseRequest destination
-    forM_ packs $ \pack -> do
-      response <- H.responseBody <$> H.httpLbs httpRequest
-        { H.method = "POST"
-        , H.requestHeaders = [("Content-Type", "application/json")]
-        , H.path = "/_bulk"
-        , H.requestBody = H.RequestBodyLBS $ elasticExportStoragePack options pack
-        } httpManager
-      errors <- either fail return . J.parseEither (J..: "errors") =<< either fail return (J.eitherDecode response)
-      when errors $ fail "errors while uploading to ElasticSearch"
+    let
+      exportPack _ [] = return ()
+      exportPack i pack = if i < (3 :: Int)
+        then do
+          response <- H.responseBody <$> H.httpLbs httpRequest
+            { H.method = "POST"
+            , H.requestHeaders = [("Content-Type", "application/json")]
+            , H.path = "/_bulk"
+            , H.requestBody = H.RequestBodyLBS $ elasticExportStoragePack options pack
+            } httpManager
+          items <- either fail return . J.parseEither (J..: "items") =<< either fail return (J.eitherDecode response)
+          packToRetry <- fmap concat . forM (zip pack items) $ \(block, item) -> do
+            maybeResult <- either fail return $ J.parseEither ((J..:? "result") <=< (J..: (if upsert then "index" else "create"))) item
+            return $ if (maybeResult :: Maybe T.Text) == Just "created" || (upsert && maybeResult == Just "updated")
+              then []
+              else [block]
+          exportPack (i + 1) packToRetry
+        else fail "too many retries when exporting to ElasticSearch"
+    mapM_ (exportPack 0) packs
 
 newtype ElasticFileExportStorage = ElasticFileExportStorage ExportStorageOptions
 
